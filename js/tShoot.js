1c65075a3d30c45e20f353561948371a6c687392
bvbrandon <bvbrandonbv@gmail.com>
2014-12-08 20:56:10 -0500

    changed for pat

---
+++ js/tShoot.js
@@ -0,0 +1,13 @@
+
+  var app = angular.module('tShoot', []);
+
+  app.controller('UrlController', function () {
+    this.site = broken;
+  })
+
+  var broken =
+  {
+    url: 'www.test.com',
+    ip: 888,
+  }
+  
